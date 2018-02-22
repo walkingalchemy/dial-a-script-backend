daec709c3b787f5df46b5b4026e213a72de85ff6
walkingalchemy <walkingalchemy@gmail.com>
2018-02-22 18:55:19 -0500

    Validate Organization has name and create test

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without a name" do
+    org = Organization.new
+    assert_not org.save
+  end
 end
