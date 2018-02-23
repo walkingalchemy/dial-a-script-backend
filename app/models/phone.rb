7de966d015a2924bad889d55ef2d21d47a1faaa9
walkingalchemy <walkingalchemy@gmail.com>
2018-02-22 19:28:07 -0500

    Validate Phone has digits and office and create tests

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -3,4 +3,8 @@ class Phone < ApplicationRecord
   has_many :phone_scripts
   has_many :scripts, through: :phone_scripts
   has_one :organization, through: :office
+
+  validates :digits, :office, presence: true
+
+
 end
--- test/models/phone_test.rb
+++ test/models/phone_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PhoneTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without digits" do
+    phone = Phone.new
+    assert_not phone.save, "Phone saved without digits"
+  end
+
+  test "should not save without office" do
+    phone = Phone.new(digits: "+1-000-000-0000")
+    assert_not phone.save, "Phone saved without office"
+  end
 end
