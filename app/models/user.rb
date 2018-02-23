125db04764fd26e3ee6cd80a82d334513327824d
walkingalchemy <walkingalchemy@gmail.com>
2018-02-22 19:34:18 -0500

    Validate User has name and it is unique and create tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :scripts
   has_many :calls
   has_many :organizations, through: :calls
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without name" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should not save without a unique name" do
+    User.create(name: "UniquenessTester")
+    user = User.new(name: "UniquenessTester")
+    assert_not user.save
+  end
 end
